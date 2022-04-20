02568e17b8ac024e7748ad006f20b65eedb55946
Nodens-Ancient <60713065+Nodens-Ancient@users.noreply.github.com>
2022-04-21 02:33:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+IWebDriver driver;
+By destinationFieldXPath = By.XPath("//input[@id='destination']");
+By reverseButtonXPath = By.XPath("//div[@data-test-id='swap-places']");
+
+driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://www.aviasales.by");
+driver.Manage().Window.Maximize();
+
+IWebElement destinationField = driver.FindElement(destinationFieldXPath);
+IWebElement reverseButton = driver.FindElement(reverseButtonXPath);
+
+destinationField.SendKeys("Omsk");
+Thread.Sleep(300);
+reverseButton.Click();
+//Console.WriteLine("elemtext " + element.GetAttribute("type"));
